9faa2f2871a10b4168c9842fa492bb32d3e59bd8
Jennifer Manriquez <jennymanb@gmail.com>
2021-08-09 23:23:50 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'JavaScript heading';
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
